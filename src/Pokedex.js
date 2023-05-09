c0133f5386a05ba68459759cdc9eebf0a73ce0fd
Rajayne <ryann.j.nealey@gmail.com>
2023-05-09 15:03:33 -0700

    Add Pokemon images to each card

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,15 @@
 import Pokecard from "./Pokecard";
 
+const image =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
 const Pokedex = ({ pokemon }) => {
   return (
     <div>
       {pokemon.map((p) => (
         <Pokecard
           key={p.id}
+          img={`${image}${p.id}.png`}
           name={p.name}
           type={p.type}
           basexp={p.base_experience}
